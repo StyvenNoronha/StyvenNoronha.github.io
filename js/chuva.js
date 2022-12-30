254bf926b692a34f6e0b0c99f6170c4aa874301e
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2022-12-30 19:51:31 -0300

    Update chuva.js

--- js/chuva.js
+++ js/chuva.js
@@ -19,3 +19,13 @@ function rain(){
     }
     
   rain()
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3RDKWQM1HH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3RDKWQM1HH');
+</script>
